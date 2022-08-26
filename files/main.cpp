ab5fe958a2839b6d64b0ef1a9115d32423ab6002
ma-lacroix <lacroix.marcantoine@gmail.com>
2022-08-26 08:59:19 +0200

    additional triangles

--- files/main.cpp
+++ files/main.cpp
@@ -11,7 +11,8 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VBO, VAO, shader, uniformModel;
+GLuint shader, uniformModel;
+unsigned int VBOs[3], VAOs[3];
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -21,50 +22,85 @@ float triIncrement = 0.0005f;
 float curAngle = 0.0f;
 
 // Vertex Shader code
-static const char* vShader = "                                                \n\
-#version 330                                                                  \n\
-                                                                              \n\
-layout (location = 0) in vec3 pos;											  \n\
-                                                                              \n\
-uniform mat4 model;                                                           \n\
-                                                                              \n\
-void main()                                                                   \n\
-{                                                                             \n\
-    gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);		  \n\
+static const char* vShader = "                                              \n\
+#version 330                                                                \n\
+                                                                        	\n\
+layout (location = 0) in vec3 pos;											\n\
+layout (location = 1) in vec3 aColor;										\n\
+uniform mat4 model;                                                         \n\
+out vec3 ourColor; 															\n\
+                                                                            \n\
+void main()                                                                 \n\
+{                                                                           \n\
+    gl_Position = model * vec4(0.5 * pos.x, 0.5 * pos.y, pos.z, 1.0);		\n\
+	ourColor = aColor;														\n\
 }";
 
 // Fragment Shader
-static const char* fShader = "                                                \n\
-#version 330                                                                  \n\
-                                                                              \n\
-out vec4 colour;                                                               \n\
-                                                                              \n\
-void main()                                                                   \n\
-{                                                                             \n\
-    colour = vec4(1.0, 0.0, 0.0, 1.0);                                         \n\
+static const char* fShader = "                                              \n\
+#version 330                                                                \n\
+                                                                            \n\
+out vec4 colour;                                                            \n\
+in vec3 ourColor; 														\n\
+                                                                            \n\
+void main()                                                                 \n\
+{                                                                           \n\
+    colour = vec4(ourColor, 1.0f);                                  			\n\
 }";
 
 void CreateTriangle()
 {
-	GLfloat vertices[] = {
-		-1.0f, -1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f,
-		0.0f, 1.0f, 0.0f
-	};
+	float firstTriangle[] = {
+        // left
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.9f,
+        // right
+		 0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 0.9f,
+		// top 
+        0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.9f,
+    };
+    float secondTriangle[] = {
+        // left
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.9f, 0.0f,
+        // right
+		 0.5f, -0.5f, 0.0f,  0.0f, 0.9f, 0.0f,
+        // top 
+		-0.5f, 0.5f, 0.0f,  0.0f, 0.9f, 0.0f
+    };
+	float thirdTriangle[] = {
+        // left 
+		-0.5f, -0.5f, -0.5f, 1.0f, 0.1f, 0.1f,  
+        // right 
+		 -0.5f, 0.5f, 0.0f,  1.0f, 0.1f, 0.1f,
+        // top 
+		0.5f, -0.5f, -0.5f,  1.0f, 0.1f, 0.1f
+    };
 
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
+	glGenVertexArrays(3, VAOs);
+	glGenBuffers(3, VBOs);
+	
+	glBindVertexArray(VAOs[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);	// Vertex attributes stay the same
+    glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));	// Vertex attributes stay the same
+    glEnableVertexAttribArray(1);
 
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindVertexArray(VAOs[1]);	// note that we bind to a different VAO now
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);	// and a different VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+    glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));	// Vertex attributes stay the same
+    glEnableVertexAttribArray(1);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	glBindVertexArray(0);
+	glBindVertexArray(VAOs[2]);	// note that we bind to a different VAO now
+    glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);	// and a different VBO
+    glBufferData(GL_ARRAY_BUFFER, sizeof(thirdTriangle), thirdTriangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // because the vertex data is tightly packed we can also specify 0 as the vertex attribute's stride to let OpenGL figure it out
+    glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));	// Vertex attributes stay the same
+    glEnableVertexAttribArray(1);
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
@@ -202,29 +238,31 @@ int main()
 			direction = !direction;
 		}
 
-		curAngle += 0.01f;
+		curAngle -= 0.01f;
 		if (curAngle >= 360)
 		{
 			curAngle -= 360;
 		}
 
 		// Clear window
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
 
 		glm::mat4 model(1.0f);
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		// model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
 		model = glm::rotate(model, curAngle * toRadians * 100.0f, glm::vec3(0.0f, 1.0f, 1.0f));
-
-
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindVertexArray(0);
+		glBindVertexArray(VAOs[0]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // then we draw the second triangle using the data from the second VAO
+        glBindVertexArray(VAOs[1]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAOs[2]);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		glUseProgram(0);
 
